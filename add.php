845db307de8b8b0244c6c21dda27704cae643ba3
lanevok <web@lanevok.com>
2014-09-21 14:20:29 +0900

    v1.10

--- add.php
+++ add.php
@@ -2,29 +2,33 @@
 session_start();
 
 function setNow($name, $time){
-	$fp = fopen('standings.ini', 'a');
-	fputs($fp, "$name=$time\n");
-	fclose($fp);
+  $fp = fopen('standings.ini', 'a');
+  fputs($fp, "$name=$time\n");
+  fclose($fp);
 }
 
 function getPastTime($name){
-	$best = 10000000;
-	$array = parse_ini_file('standings.ini');
-	foreach($array as $key => $value){
-		if($key==$name){
-			$best = min($best, $value);
-		}
-	}
-	return $best;
+  $best = 10000000;
+  $array = parse_ini_file('standings.ini');
+  foreach($array as $key => $value){
+    if($key==$name){
+      $best = min($best, $value);
+    }
+  }
+  return $best;
 }
 
 if($_SESSION['ok']){
-	$_SESSION['ok'] = false;
-	$name = htmlspecialchars($_POST['name']);
-	$time = ($_SESSION['count']-1)*30+$_SESSION['time'];
-	if($time<getPastTime($name)){
-		setNow($name, $time);
-	}
-	echo "<meta http-equiv=refresh content=0;URL='./index.php'>\n";
+  $_SESSION['ok'] = false;
+  $name = htmlspecialchars($_POST['name']);
+  $name = ereg_replace("[^0-9a-zA-Z]", "", $name);
+  $time = ($_SESSION['count']-1)*30+$_SESSION['time'];
+  if($time<getPastTime($name)){
+    if(mb_strtolower($name)!="null"&&mb_strtolower($name)!="yes"&&
+       mb_strtolower($name)!="no"&&mb_strtolower($name)!="true"&&mb_strtolower($name)!="false"&&$name!=""){
+      setNow($name, $time);
+    }
+  }
+  echo "<meta http-equiv=refresh content=0;URL='./index.php'>\n";
 }
 ?>
